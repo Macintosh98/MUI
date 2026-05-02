22d62d8101a947c9027d648627cf06a2eb1e4da8
Macintosh98 <abhishekzambare@gmail.com>
2026-05-02 20:43:37 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Card } from "@abhishekzambare/mui";
+function App() {
+  return (
+    <>
+      <Card>
+        <>dgsdgsds</>
+      </Card>
+    </>
+  );
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,47 @@
+import { defineConfig, esmExternalRequirePlugin } from "vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
+import { resolve } from "node:path";
+import dts from "unplugin-dts/vite";
+import checker from "vite-plugin-checker";
+
+// https://vite.dev/config/
+export default defineConfig({
+  server: {
+    port: 3000,
+  },
+  resolve: {
+    tsconfigPaths: true,
+  },
+  plugins: [
+    react(),
+    dts({ bundleTypes: true, tsconfigPath: "./tsconfig.app.json" }),
+    babel({ presets: [reactCompilerPreset()] }),
+    checker({
+      typescript: {
+        buildMode: true,
+        tsconfigPath: "./tsconfig.app.json",
+      },
+      eslint: {
+        useFlatConfig: true,
+        watchPath: "./src/**/*.{js,jsx,ts,tsx}",
+        lintCommand: 'eslint "./src/**/*.{js,jsx,ts,tsx}"',
+      },
+    }),
+  ],
+  build: {
+    lib: {
+      entry: resolve(import.meta.dirname, "lib/main.ts"),
+      name: "Mui",
+      fileName: "mui",
+      formats: ["es"],
+    },
+    rolldownOptions: {
+      plugins: [
+        esmExternalRequirePlugin({
+          external: [/^react(-dom)?(\/.+)?$/],
+        }),
+      ],
+    },
+  },
+});
