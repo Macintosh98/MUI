8cae0464d60352b5670a6afe43b4463e42d6b303
Macintosh98 <abhishekzambare@gmail.com>
2026-07-22 20:55:17 +0530

    push

--- src/App.tsx
+++ src/App.tsx
@@ -30,6 +30,25 @@ import {
   IconButton,
   Box,
   Alert,
+  FormLabel,
+  FormControlLabel,
+  Radio,
+  RadioGroup,
+  Chip,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  CircularProgress,
+  LinearProgress,
 } from "@mui/material";
 import RowBox from "./RowBox";
 import AddIcon from "@mui/icons-material/Add";
@@ -45,9 +64,38 @@ import MailIcon from "@mui/icons-material/Mail";
 import InboxIcon from "@mui/icons-material/Inbox";
 import DraftsIcon from "@mui/icons-material/Drafts";
 import DeleteIcon from "@mui/icons-material/Delete";
+import React from "react";
 
 function App() {
   const theme = useMUIThemeContext();
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  function createData(
+    name: string,
+    calories: number,
+    fat: number,
+    carbs: number,
+    protein: number,
+  ) {
+    return { name, calories, fat, carbs, protein };
+  }
+
+  const rows = [
+    createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+    createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+    createData("Eclair", 262, 16.0, 24, 6.0),
+    createData("Cupcake", 305, 3.7, 67, 4.3),
+    createData("Gingerbread", 356, 16.0, 49, 3.9),
+  ];
 
   return (
     <ThemeProvider theme={theme.Theme_WebApp_Theme}>
@@ -162,6 +210,34 @@ function App() {
               <Fab disabled aria-label="like">
                 <FavoriteIcon />
               </Fab>,
+            ]}
+          ></RowBox>
+        </Card>
+
+        <Card sx={{ p: 3, m: 3 }}>
+          <RowBox
+            startElement={[<Typography variant="h3">Radio Group</Typography>]}
+            endElement={[
+              <FormControl>
+                <FormLabel>Gender</FormLabel>
+                <RadioGroup defaultValue="female" name="radio-buttons-group">
+                  <FormControlLabel
+                    value="female"
+                    control={<Radio />}
+                    label="Female"
+                  />
+                  <FormControlLabel
+                    value="male"
+                    control={<Radio />}
+                    label="Male"
+                  />
+                  <FormControlLabel
+                    value="other"
+                    control={<Radio />}
+                    label="Other"
+                  />
+                </RadioGroup>
+              </FormControl>,
             ]}
           ></RowBox>
         </Card>
@@ -334,6 +410,16 @@ function App() {
 
         <Card sx={{ p: 3, m: 3 }}>
           <RowBox
+            startElement={[<Typography variant="h3">Chip</Typography>]}
+            endElement={[
+              <Chip label="Chip Filled" />,
+              <Chip label="Chip Outlined" variant="outlined" />,
+            ]}
+          ></RowBox>
+        </Card>
+
+        <Card sx={{ p: 3, m: 3 }}>
+          <RowBox
             startElement={[<Typography variant="h3">List</Typography>]}
             endElement={[
               <List>
@@ -354,6 +440,44 @@ function App() {
                   </ListItemButton>
                 </ListItem>
               </List>,
+            ]}
+          ></RowBox>
+        </Card>
+        <Card sx={{ p: 3, m: 3 }}>
+          <RowBox
+            startElement={[<Typography variant="h3">Table</Typography>]}
+            endElement={[
+              <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>Dessert (100g serving)</TableCell>
+                      <TableCell align="right">Calories</TableCell>
+                      <TableCell align="right">Fat&nbsp;(g)</TableCell>
+                      <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+                      <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {rows.map((row) => (
+                      <TableRow
+                        key={row.name}
+                        sx={{
+                          "&:last-child td, &:last-child th": { border: 0 },
+                        }}
+                      >
+                        <TableCell component="th" scope="row">
+                          {row.name}
+                        </TableCell>
+                        <TableCell align="right">{row.calories}</TableCell>
+                        <TableCell align="right">{row.fat}</TableCell>
+                        <TableCell align="right">{row.carbs}</TableCell>
+                        <TableCell align="right">{row.protein}</TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>,
             ]}
           ></RowBox>
         </Card>
@@ -439,6 +563,58 @@ function App() {
               <Alert severity="info">This is an info Alert.</Alert>,
               <Alert severity="warning">This is a warning Alert.</Alert>,
               <Alert severity="error">This is an error Alert.</Alert>,
+            ]}
+          ></RowBox>
+        </Card>
+        <Card sx={{ p: 3, m: 3 }}>
+          <RowBox
+            startElement={[<Typography variant="h3">Dialog</Typography>]}
+            endElement={[
+              <React.Fragment>
+                <Button variant="outlined" onClick={handleClickOpen}>
+                  Open alert dialog
+                </Button>
+                <Dialog
+                  open={open}
+                  onClose={handleClose}
+                  aria-labelledby="alert-dialog-title"
+                  aria-describedby="alert-dialog-description"
+                  role="alertdialog"
+                >
+                  <DialogTitle id="alert-dialog-title">
+                    {"Use Google's location service?"}
+                  </DialogTitle>
+                  <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                      Let Google help apps determine location. This means
+                      sending anonymous location data to Google, even when no
+                      apps are running.
+                    </DialogContentText>
+                  </DialogContent>
+                  <DialogActions>
+                    <Button onClick={handleClose} autoFocus>
+                      Disagree
+                    </Button>
+                    <Button onClick={handleClose}>Agree</Button>
+                  </DialogActions>
+                </Dialog>
+              </React.Fragment>,
+            ]}
+          ></RowBox>
+        </Card>
+
+        <Card sx={{ p: 3, m: 3 }}>
+          <RowBox
+            startElement={[<Typography variant="h3">Progress</Typography>]}
+            endElement={[
+              <CircularProgress aria-label="Loading…" />,
+              <CircularProgress color="secondary" aria-label="Loading…" />,
+              <CircularProgress color="success" aria-label="Loading…" />,
+              <CircularProgress color="inherit" aria-label="Loading…" />,
+              <LinearProgress aria-label="Loading…" />,
+              <LinearProgress color="secondary" aria-label="Loading…" />,
+              <LinearProgress color="success" aria-label="Loading…" />,
+              <LinearProgress color="inherit" aria-label="Loading…" />,
             ]}
           ></RowBox>
         </Card>
